8442ef0db293a9dda6af134791cb7354c76928a6
LuciSkroder <Luci.skroder@gmail.com>
2025-01-26 14:52:07 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+let birthYear = prompt (`Please enter your birth year`);
+let birthMonth = prompt (`Please enter your birth month (as a number)`);
+let birthDay = prompt (`Please enter your birth day`);
+
+let year = new Date().getFullYear();
+let month = new Date().getMonth() + 1;
+let day = new Date().getDate();
+
+let age = year - parseInt(birthYear);
+
+console.log("day: " + day);
+console.log("birthDay: " + birthDay);
+
+if (month - birthMonth < 0 || (month - birthMonth === 0 && day < birthDay)) {
+    age -= 1;
+}
+
+console.log(age);
